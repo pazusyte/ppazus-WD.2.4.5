9ba186ddd965736015f0c59af717e645e5fe5150
Patricija Pazusyte <patricija.as@gmail.com>
2024-04-29 20:26:38 +0300

    chore:	updated to typescript

--- src/components/dates/dateHelpers.ts
+++ src/components/dates/dateHelpers.ts
@@ -1,7 +1,7 @@
 export function generateOptions(start: number, end: number) {
-  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i)
 }
 
 export function padZero(value: any) {
-  return value < 10 ? `0${value}` : value.toString();
+  return value < 10 ? `0${value}` : value.toString()
 }
